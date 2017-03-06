e8051c895eada9290578e7b7ccbb9778a3600fd1
Kevin <kevin@shortcut.nl>
2017-03-06 17:23:54 +0100

    Sat page edit

--- wp-content/themes/crystalskull/page-satellites.php
+++ wp-content/themes/crystalskull/page-satellites.php
@@ -2,12 +2,12 @@
  /*
  * Template Name: Satellites
  */
- include 'satellite_array.php';
-  $user_ID = get_current_user_id();
-  $sat_level = get_user_meta($user_ID, 'level_satellite_construction',true);
-  $sat_owned = get_user_meta($user_ID, 'sat_owned',true);
-  $sat_progress = get_user_meta($user_ID, 'sat_in_progress',true);
-  $sat_endlife = get_user_meta($user_ID, 'sat_endlife',true);
+include 'satellite_array.php';
+$user_ID = get_current_user_id();
+$sat_level = get_user_meta($user_ID, 'level_satellite_construction',true);
+$sat_owned = get_user_meta($user_ID, 'sat_owned',true);
+$sat_progress = get_user_meta($user_ID, 'sat_in_progress',true);
+$sat_endlife = get_user_meta($user_ID, 'sat_endlife',true);
 get_header(); ?>
 <div class="page normal-page">
      <div class="container">
@@ -32,50 +32,59 @@ get_header(); ?>
 				<div class="marketnotice insuffunds">Enter a valid number</div>
 			<?php endif;?><?php endif;?>
 			
-			<?php if(get_field('game_status','option') != 'Live'):?>
-			<div class="notice_message"><span class="rdw-line">The round has ended!</span></div>
-			<?php else:?>
+
+
+<?php if(get_field('game_status','option') != 'Live'):?>
+	<div class="notice_message"><span class="rdw-line">The round has ended!</span></div>
+<?php else:?>
 			
-			<?php if($sat_level == '0'):?>
-			<div class="notice_message"><span class="rdw-line"><a style="color:#fff;"href="/research/">Research satellite construction</a></span></div>
-			<?php endif;?>
+
+
+
+<?php if($sat_level == '0'):?>
+	<div class="notice_message"><span class="rdw-line"><a style="color:#fff;"href="/research/">Research satellite construction</a></span></div>
+<?php endif;?>
 			
 			
-			<?php if($sat_progress != '0'):?>
+<?php if($sat_progress != '0'):?>
 			
-			<table class="responsive-table">
-					<thead>
-						<tr>
-						<th scope="col">Name</th>
-						<th scope="col">Amount</th>
-						<th scope="col">Time left</th>
-  					</tr>
-					</thead>
-					<tbody>
-			<?php 	
+	<table class="responsive-table">
+		<thead>
+			<tr>
+				<th scope="col">Name</th>
+				<th scope="col">Amount</th>
+				<th scope="col">Time left</th>
+  			</tr>
+		</thead>
+		
+		<tbody>
+			
+			
+<?php 	
 	
-		$args = array(
+$args = array(
 	'posts_per_page'   => -1,
 	'meta_key'		=> 'user_placed_id',
 	'meta_value'	=> get_current_user_ID(),
 	'post_type'        => 'market_order',
 	'meta_query'	=> array(
-                    'relation' => 'OR',
-					array(
-                             'key' => 'user_placed_id',
-                             'value' => get_current_user_ID(),
-                             'compare' => 'IN'
-                           ),
-                           array(
-                             'key' => 'order_type',
-                             'value' => 'satellite',
-                             'compare' => 'IN'
-                           ),
-                         )
-	);
-	$units = get_posts( $args ); 
+		'relation' => 'OR',
+		array(
+			'key' => 'user_placed_id',
+			'value' => get_current_user_ID(),
+			'compare' => 'IN'
+			),
+		array(
+			'key' => 'order_type',
+			'value' => 'satellite',
+			'compare' => 'IN'
+			),
+		)
+);
+	
+$units = get_posts( $args ); 
 
-	$timestamp = strtotime(date('Y-m-d H:i:s'));
+$timestamp = strtotime(date('Y-m-d H:i:s'));
 	
 	foreach ($units as $order) {
 		$units_in_this_order = get_post_meta($order->ID,'amount_ordered',true);
@@ -127,7 +136,7 @@ get_header(); ?>
 			
 
 			
-			<?php if($sat_level != 0 && $sat_owned != 'laser' && $sat_progress != 'laser'):?>
+			<?php if($sat_level != '0' && $sat_owned == '0' && $sat_progress == '0'):?>
 			<div class="notice_message"><span class="rdw-line">Building a satellite requires 25 turns</span></div><br/>
 			<form class="form" action="<?php echo home_url() ?>/satellite.php" name="" id="satellite" method="post">
 				
@@ -175,7 +184,7 @@ get_header(); ?>
 
 		<?php endif;?>
 		
-		<?php if($sat_owned == 'laser'):?>
+		<?php if($sat_owned != '0'):?>
 		<div class="notice_message"><span class="rdw-line">You currently own a <?php echo $satellites[$sat_owned]['name'];?></span>
 		<?php 
 			$timestamp = strtotime(date('Y-m-d H:i:s'));
