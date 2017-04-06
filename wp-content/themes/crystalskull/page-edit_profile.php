aad913946c1a52a487cb7fc0b6a29262903133b3
assault.online <assauu@dedi1630.your-server.de>
2017-04-06 10:07:29 +0200

    profile edit tel input field

--- wp-content/themes/crystalskull/page-edit_profile.php
+++ wp-content/themes/crystalskull/page-edit_profile.php
@@ -88,7 +88,7 @@ get_header(); ?>
 				<div class="col-xs-7">
 					<div class="input-group input-group-sm">
 						<span class="input-group-addon" id="sizing-addon3"><i class="fa fa-mobile fa-lg" aria-hidden="true"></i></span>
-						<input type="number" value="<?php echo $phone_number;?>" type="text" class="form-control" placeholder="example: 316123456789" name="phonenumber" aria-describedby="sizing-addon3">
+						<input type="tel" value="<?php echo $phone_number;?>" type="text" class="form-control" placeholder="example: 316123456789" name="phonenumber" aria-describedby="sizing-addon3">
 					</div>
 				
 				</div>
