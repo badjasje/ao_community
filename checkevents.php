2a9516b549b48189fc18de46ae9c9dbb04854053
Kevin <kevin@shortcut.nl>
2017-05-22 13:51:43 +0200

    Checking for new globals every 10 seconds

---
+++ checkevents.php
@@ -0,0 +1,13 @@
+<?php
+	
+	require_once("wp-load.php");
+	$user_ID = get_current_user_id();
+
+	$globals = get_user_meta($user_ID, 'new_global_events', true);	
+	if($globals > 0){
+	echo '('.$globals . ') global event'.plural_func($globals).' - Assault.Online';
+	}else{
+	
+		echo 'Assault.Online';
+	}
+	
--- wp-content/themes/crystalskull/footer.php
+++ wp-content/themes/crystalskull/footer.php
@@ -5,11 +5,6 @@
 		<br/><a href="<?php echo wp_logout_url( get_permalink(3491) ); ?>">Logout</a>
 		<?php endif;?>
 		</center>
-		<?php if ( function_exists( 'dynamic_sidebar' ) && is_active_sidebar( 'footer' ) ) : ?>
-
-			<?php dynamic_sidebar( 'footer' ); ?>
-
-		<?php endif; ?>
 
 	</div>
 </footer>
@@ -18,33 +13,6 @@
 	<div class="container">
 
 	<p>© <?php echo date("Y"); ?>&nbsp;
-
-		<?php if(of_get_option('copyright')!=""){
-			 echo of_get_option('copyright'); ?>
-		<?php } ?>
-
-	<?php if(of_get_option('terms')!= "" or of_get_option('termsname') != ""){ ?> || <?php } ?>
-
-
-	<?php if(of_get_option('terms') != ""){?> <a href="<?php echo esc_url(of_get_option('terms')); ?>"> <?php } ?>
-
-	<?php if(of_get_option('termsname')!=""){ echo of_get_option('termsname');} ?>
-
-	<?php if(of_get_option('terms')!=""){?> </a> <?php } ?>
-	&nbsp;
-
-	<div class="social">
-	<?php if ( of_get_option('rss') ) { ?> <a class="rss" target="_blank" href="<?php  echo esc_url(of_get_option('rss_link'));  ?>"><i class="fa fa-rss"></i> </a><?php } ?>
-	<?php if ( of_get_option('dribbble') ) { ?> <a class="dribbble" target="_blank" href="<?php  echo esc_url(of_get_option('dribbble_link'));  ?>"><i class="fa fa-dribbble"></i> </a><?php } ?>
-	<?php if ( of_get_option('vimeo') ) { ?> <a class="vimeo" target="_blank" href="<?php echo esc_url(of_get_option('vimeo_link'));   ?>"><i class="fa fa-vimeo-square"></i> </a><?php } ?>
-	<?php if ( of_get_option('youtube') ) { ?> <a class="youtube" target="_blank" href="<?php echo esc_url(of_get_option('youtube_link'));   ?>"><i class="fa fa-youtube"></i> </a><?php } ?>
-	<?php if ( of_get_option('twitch') ) { ?> <a class="twitch" target="_blank" href="<?php echo esc_url(of_get_option('twitch_link'));   ?>"><i class="fa fa-twitch"></i></a><?php } ?>
-	<?php if ( of_get_option('steam') ) { ?> <a class="steam" target="_blank" href="<?php echo esc_url(of_get_option('steam_link'));   ?>"><i class="fa fa-steam"></i></a><?php } ?>
-	<?php if ( of_get_option('pinterest') ) { ?> <a class="pinterest" target="_blank" href="<?php  echo esc_url(of_get_option('pinterest_link'));   ?>"><i class="fa fa-pinterest"></i> </a><?php } ?>
-	<?php if ( of_get_option('googleplus') ) { ?> <a class="google-plus" target="_blank" href="<?php echo esc_url(of_get_option('google_link'));   ?>"><i class="fa fa-google-plus"></i></a><?php } ?>
-	<?php if ( of_get_option('twitter') ) { ?> <a class="twitter" target="_blank" href="<?php  echo esc_url(of_get_option('twitter_link'));   ?>"><i class="fa fa-twitter"></i></a><?php } ?>
-	<?php if ( of_get_option('facebook') ) { ?> <a class="facebook" target="_blank" href="<?php echo esc_url(of_get_option('facebook_link'));   ?>"><i class="fa fa-facebook"></i></a><?php } ?>
-	</div>
 
 	</div>
 </div>
@@ -63,7 +31,24 @@
 jQuery( ".menu-item-7706" ).append( "<span class='bluepulse'><a class='bluepulse' href='/events/global/'><?php echo $new_globals;?> new globals</a></span>" );
 </script>
 <?php endif; */?>
+<script>
+	(function($) {
+	function myFunction() {
+		$.get('<?php echo get_site_url();?>/checkevents.php', function(data) {
+			var newevents = data; 
+		    $('title').text(newevents);
+			
 
+            
+            
+            });
+
+    }
+
+var i = setInterval(function() { myFunction(); }, 10000);
+
+})(jQuery);
+</script>
 
 <?php if(!empty($new_events) and $new_events != 0):?>
 <script type="text/javascript">
