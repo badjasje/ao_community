2c469af193a3f222f0c75c50f1f3e13a889a8bad
Kevin <kevin@shortcut.nl>
2017-08-24 10:21:07 +0200

    Adapt buttons for prev/next in event pages

--- wp-content/plugins/marketorders/index.php
+++ wp-content/plugins/marketorders/index.php
@@ -16,6 +16,13 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+add_filter('next_posts_link_attributes', 'posts_link_attributes');
+add_filter('previous_posts_link_attributes', 'posts_link_attributes');
+
+function posts_link_attributes() {
+    return 'class="btn btn-general"';
+}
+
 function unit_types($user_ID){
 	include('units_array.php');
 	$type_array = array();
