db972f16313d8edb2699bb6bf9a6eeb50807c763
Elon Musk <rubenroeffaers@hotmail.com>
2018-01-14 22:53:56 +0100

    included total land in clan networth overview

--- wp-content/themes/crystalskull/pages/toplist/clan_nw.php
+++ wp-content/themes/crystalskull/pages/toplist/clan_nw.php
@@ -1,15 +1,16 @@
 <div class="tab-pane <?php echo $activeTab === 'clannw' ? 'active' : ''; ?>" id="clannw" role="tabpanel" >
 
-<div class="row toplist_block">	
+<div class="row toplist_block">
 	<div class="row clan_header_row storeDetails-heads">
 		<div class="col-md-1"></div>
 		<div class="col-md-1"></div>
 		<div class="col-md-6"><strong>Name</strong></div>
-		<div class="col-md-4"><strong>Clan networth</strong></div>
+		<div class="col-md-2"><strong>Land</strong></div>
+		<div class="col-md-2"><strong>Networth</strong></div>
 
 	</div>
-	
-	
+
+
 		<?php
 
 		$position = 0;
@@ -25,44 +26,56 @@
 			$clan_members = get_post_meta($clan->ID,'clan_members');
 
 			$tot_networth = 0;
+			$tot_land = 0;
 			foreach ($clan_members[0] as $member) {
-				$networth = get_user_meta($member, 'networth');
-				$tot_networth+=$networth[0];}
+				$tot_networth += get_user_meta($member, 'networth')[0];
+				$tot_land     += get_user_meta($member, 'land', true);
+            }
 			update_post_meta($clan->ID,'clan_networth',ceil($tot_networth));
+			update_post_meta($clan->ID,'clan_land',    ceil($tot_land));
 			?>
-			
+
 <div class="row clan_profile_row2">
-		
+
 	<div class="col-md-1">
 		<div class="positionNo">
 			<?php echo $position+=1;?>
 		</div>
 	</div>
-	
-	
+
+
 	<div class="col-md-1">
 		<?php echo clan_avatar($clan->ID,'');?>
 	</div>
-	
-	
-	<div class="col-md-6 clan_column center_clan_col border_bottom_mobile">
-		<a href="<?php echo get_permalink($clan); ?>"><?php echo $clan->post_title.' (#'.$clan->ID.')';?></a>		
-	</div>
-	
-	
 
-	
-	<div class="col-md-4 clan_column">
-		
-		<span class="clan_data_left">Clan networth</span>
+
+	<div class="col-md-6 clan_column center_clan_col border_bottom_mobile">
+		<a href="<?php echo get_permalink($clan); ?>"><?php echo $clan->post_title.' (#'.$clan->ID.')';?></a>
+	</div>
+
+	<div class="col-md-2 clan_column">
+
+		<span class="clan_data_left">Land</span>
+		<span class="clan_data_right store-pop-span2">
+			<?php echo number_format(get_post_meta($clan->ID, 'clan_land',true), 0, ',', ' ')?> m&#178;
+		</span>
+
+	</div>
+
+	<div class="col-md-2 clan_column">
+
+		<span class="clan_data_left">Networth</span>
 		<span class="clan_data_right store-pop-span2">
 			$ <?php echo number_format(get_post_meta($clan->ID, 'clan_networth',true), 0, ',', ' ')?>
 		</span>
-	
+
 	</div>
+
+
+
 </div> <! // Close profile row -- >
-			
-			
+
+
 
 		<?php }?>
 	</div>
