fe3b605238bad76fa003690f08cad0a7775a4fd8
Elon Musk <rubenroeffaers@hotmail.com>
2018-02-12 22:09:44 +0100

    fix awardlist on mobile and notice for nonexisting index and bugfix

--- wp-content/themes/crystalskull/single-clan.php
+++ wp-content/themes/crystalskull/single-clan.php
@@ -1,5 +1,5 @@
 <?php
-include('constants.php'); 
+include('constants.php');
 $declarer_ID = get_current_user_ID();
 update_user_meta($declarer_ID, 'user_lock', 0);
 $userData = get_user_meta($declarer_ID);
@@ -35,7 +35,7 @@ $decct_3 = $declarerClanData['ct_3'][0];
 $decct_4 = $declarerClanData['ct_4'][0];
 
 $allowed_to_declare = array($declarer_clanleader,$decct_1,$decct_2,$decct_3,$decct_4);
-				
+
 $warcount = get_posts(array(
 	'numberposts'	=> -1,
 	'post_type'		=> 'wars',
@@ -80,8 +80,8 @@ if($declarer_clan_ID != 0){
 foreach ($wars_on as $war) {
 	$defClanID = get_post_meta($war->ID,'declared_on',true);
 	$att_ClanID = get_post_meta($war->ID,'declared_by',true);
-	
-	
+
+
 	if($defClanID == $clan_id){
 		$peaceID = $war->ID;
 	}
@@ -91,7 +91,7 @@ $_member = false;
 
 if(in_array($declarer_ID, $declaringClanMembers)){
 	$_member = true;
-	
+
 }
 
 get_header(); ?>
@@ -114,43 +114,43 @@ get_header(); ?>
 		$clanleader = $clanData['clan_leader'][0];
 		$clan_points = $clanData['clan_points'][0];
 		$clantag = $clanData['clan_tag'][0];
-		
+
 		$tot_networth = 0;
 		foreach ($clanMembers as $member) {
 
 			count_all_stats($member);
 			$networth = get_user_meta($member, 'networth',true);
 			$tot_networth+=$networth;
-		
+
 		}
-			
+
 		update_post_meta($clan_id, 'clan_networth', ceil($tot_networth));
-			
-				
+
+
 		while ( have_posts() ) : the_post(); ?>
 		<?php if(!empty($clanData['clan_image'][0])):?>
-		<div class="row profile_block">	
-		
+		<div class="row profile_block">
+
 			<center>
 			<div style="width:100%; height:300px;background: url('<?php echo $clanData['clan_image'][0]; ?>') center center;background-repeat: no-repeat;">
 			</center>
-		
+
 		</div>
 		<?php endif;?>
-		
+
 		<div class="row profile_block">
 			<div class="row">
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Name</div>
 					<div class="col-xs-7"><?php echo get_the_title($clan_id);?></div>
 				</div>
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Members</div>
 					<div class="col-xs-7"><?php echo count($clanMembers);?></div>
 				</div>
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Awards</div>
 					<div class="col-xs-7">
@@ -160,17 +160,17 @@ get_header(); ?>
                         <a id="awardlistExpandBtn" style="display: none">Show more</a>
 					</div>
 				</div>
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Tag</div>
 					<div class="col-xs-7"><?php echo $clantag;?></div>
 				</div>
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Total networth</div>
 					<div class="col-xs-7">$ <?php echo number_format($tot_networth, 0, ',', ' ');?></div>
 				</div>
-				
+
 				<div class="row profile_row">
 					<div class="col-xs-5">Average networth</div>
 					<div class="col-xs-7">$ <?php echo number_format($averageNw, 0, ',', ' ');?></div>
@@ -185,7 +185,7 @@ get_header(); ?>
 								echo '0';}?>pts <sup><?php echo $clanData['24h_pts'][0];?>pts today</sup>
 					</div>
 				</div>
-				
+
 				<div class="row profile_row_last">
 					<div class="col-xs-5">Message</div>
 					<div class="col-xs-7">
@@ -213,7 +213,7 @@ get_header(); ?>
 <script type="text/javascript" src="/wp-content/themes/crystalskull/js/aoReadMore.js"></script>
 <script type="text/javascript">
     initReadMore("clanMessage", "clanMessageExpandBtn", 54);
-    initReadMore("awardlist",   "awardlistExpandBtn",   163);
+    initReadMore("awardlist",   "awardlistExpandBtn",   164);
 </script>
 
 
@@ -229,22 +229,22 @@ get_header(); ?>
 		$inRange = 'yes';
 		$warText = 'mutual war';
 	}
-	// can peace clan? 
+	// can peace clan?
 	$canPeace = false;
 	if($peaceID != 0 && ($timestamp-get_the_title($peaceID) > 86400)){
 		$canPeace = true;
 	}
-	
-	?>		
-		
-		
-		
-		
-		
-		
-		
 
-		
+	?>
+
+
+
+
+
+
+
+
+
 <?php if(!in_array($declarer_ID, $clanMembers)):?>
 
 
@@ -252,13 +252,13 @@ get_header(); ?>
 <!-- Enemy clan block -->
 <div class="storeDetails-heads button_block sortingHeadMob">
 	<center>
-	<strong>Sort:</strong> <a href="" class="sort" data-sort=".memberField">Name</a> - 
+	<strong>Sort:</strong> <a href="" class="sort" data-sort=".memberField">Name</a> -
 	<a href="" class="sort sort-number" data-sort=".store-pop-span2">Networth</a> -
 	<a href="" class="sort sort-number" data-sort=".land">Land</a>
 	</center>
 </div>
 
-<div class="row profile_block storeDetails-heads">	
+<div class="row profile_block storeDetails-heads">
 <div class="row clan_header_row ">
 	<div class="col-md-1"></div>
 	<div class="col-md-4"><strong><a href="" class="sort" data-sort=".memberField">Name</a></strong></div>
@@ -267,7 +267,7 @@ get_header(); ?>
 	<div class="col-md-2"></div>
 </div>
 <div id="values">
-<?php 
+<?php
 	$NRmembers = count($clanMembers);
 	$counter = 0;
 	foreach ($clanMembers as $key => $member) {
@@ -275,7 +275,7 @@ get_header(); ?>
 		$counter++;
 		if($counter == $NRmembers){
 			$extraClass = 'clan_profile_row_last';
-			
+
 		}
 		$member_data = get_userdata($member);
 		$memberMetaData = get_user_meta($member);
@@ -290,8 +290,8 @@ get_header(); ?>
 <div class="row clan_profile_row">
 	<div class="col-md-1">
 		<?php echo small_avatar($member,'');?>
-		
-		
+
+
 	</div>
 	<div class="col-md-4 clan_column center_clan_col border_bottom_mobile">
 		<div class="ctclField">
@@ -303,9 +303,9 @@ get_header(); ?>
 			} ?>
 			</div>
 		<?php echo get_user_name($member);?>
-					
-			
-			
+
+
+
 	</div>
 	<div class="col-md-3 clan_column border_bottom_mobile">
 		<span class="clan_data_left">Networth</span>
@@ -320,9 +320,9 @@ get_header(); ?>
 		<?php echo number_format($land, 0, ',', ' '); ?> m<sup>2</sup>
 		</span>
 	</div>
-	
+
 	<div class="col-md-2 clan_column center_clan_col">
-		<a href="/attack/step-1/?id=<?php echo $member;?>"><i class="fa fa-crosshairs fa-lg" aria-hidden="true"></i></a> 
+		<a href="/attack/step-1/?id=<?php echo $member;?>"><i class="fa fa-crosshairs fa-lg" aria-hidden="true"></i></a>
 		<a href="/spy-reports/?id=<?php echo $member;?>"><i class="fa fa-binoculars" aria-hidden="true"></i></a>
 	</div>
 </div>
@@ -335,7 +335,7 @@ get_header(); ?>
 <?php if(in_array($declarer_ID, $allowed_to_declare) && !array_key_exists($clan_id, $cooldownlist) && $inRange == 'yes' && $canPeace == false):?>
 
 <div class="row button_block">
- 	
+
  	<div class="col-md-6 buttoncol">
 	 	<?php if (in_array($clan_id, $declared_on)):?>
 	 	<center><span class="btn btn-disabled profilebutton">
@@ -345,8 +345,8 @@ get_header(); ?>
                    <?php if ($average_OK == "false" && $warcount != 1) {
                      ?>
                     <span class="btn btn-disabled profilebutton">
-                        <i class="fa fa-fire" aria-hidden="true"></i> &nbsp;Your average NW is too low to declare on this clan</span> 
-                     <?php 
+                        <i class="fa fa-fire" aria-hidden="true"></i> &nbsp;Your average NW is too low to declare on this clan</span>
+                     <?php
                    }
                    else { ?>
                      <a class="btn btn-general profilebutton declarewar" onclick="return confirm('Are you sure you want to declare <?php echo $warText;?>?')" href="/declare_war.php?clan=<?php echo $clan_id;?>">
@@ -356,14 +356,14 @@ get_header(); ?>
                  </center>
 		 <?php endif;?>
 	</div>
-	
+
 	<div class="col-md-6 buttoncol">
 	 	<center><a class="btn btn-general profilebutton" href="/spy-report-overview/?id=<?php echo $clan_id;?>">
 		 	<i class="fa fa-bar-binoculars" aria-hidden="true"></i> &nbsp;View spyreports</a></center>
 	</div>
 
 </div>
-<script> 
+<script>
   jQuery(".declarewar").click(function (event) {
     if (jQuery(this).hasClass("disabled")) {
         event.preventDefault();
@@ -379,12 +379,12 @@ get_header(); ?>
 <?php if(in_array($declarer_ID, $allowed_to_declare) && $canPeace == true):?>
 <!-- Declare peace block -->
 <div class="row button_block">
- 	
- 	<div class="col-md-6 buttoncol">	
+
+ 	<div class="col-md-6 buttoncol">
 		 <center><a class="btn btn-general profilebutton" onclick="return confirm('Are you sure you want to declare peace?')" href="/declare_peace.php/?war=<?php echo $peaceID;?>">
 		 	<i class="fa fa-fire" aria-hidden="true"></i> &nbsp;Declare peace</a></center>
 	</div>
-	
+
 	<div class="col-md-6 buttoncol">
 	 	<center><a class="btn btn-general profilebutton" href="/spy-report-overview/?id=<?php echo $clan_id;?>">
 		 	<i class="fa fa-bar-binoculars" aria-hidden="true"></i> &nbsp;View spyreports</a></center>
@@ -399,12 +399,12 @@ get_header(); ?>
 <?php if(in_array($declarer_ID, $allowed_to_declare) && !array_key_exists($clan_id, $cooldownlist) &&  $inRange == 'no' && $canPeace == false):?>
 
 <div class="row button_block">
- 	
+
  	<div class="col-md-6 buttoncol">
 	 	<center><span class="btn btn-disabled profilebutton">
 		 	<i class="fa fa-fire" aria-hidden="true"></i> &nbsp;Currently not in range</span></center>
 	</div>
-	
+
 	<div class="col-md-6 buttoncol">
 	 	<center><a class="btn btn-general profilebutton" href="/spy-report-overview/?id=<?php echo $clan_id;?>">
 		 	<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;View spyreports</a></center>
@@ -419,12 +419,12 @@ get_header(); ?>
 <div class="row button_block">
 	<div class="col-md-3 buttoncol">
 	</div>
-	
+
 	<div class="col-md-6 buttoncol">
 	 	<center><a class="btn btn-general profilebutton" href="/spy-report-overview/?id=<?php echo $clan_id;?>">
 		 	<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;View spyreports</a></center>
 	</div>
-	
+
 	<div class="col-md-3 buttoncol">
 	</div>
 
@@ -448,9 +448,9 @@ get_header(); ?>
 <?php if(in_array($declarer_ID, $clanMembers)):?>
 <div class="storeDetails-heads button_block sortingHeadMob">
 	<center>
-	<strong>Sort:</strong> <a href="" class="sort" data-sort=".memberField">Name</a> - 
+	<strong>Sort:</strong> <a href="" class="sort" data-sort=".memberField">Name</a> -
 	<a href="" class="sort sort-number" data-sort=".store-pop-span2">Networth</a> -
-	<a href="" class="sort sort-number" data-sort=".land">Land</a> - 
+	<a href="" class="sort sort-number" data-sort=".land">Land</a> -
 	<a href="" class="sort sort-number" data-sort=".points">Points</a>
 	</center>
 </div>
@@ -458,7 +458,7 @@ get_header(); ?>
 <!-- Own clan block -->
 
 
-<div class="row profile_block storeDetails-heads">	
+<div class="row profile_block storeDetails-heads">
 <div class="row clan_header_row ">
 	<div class="col-md-1"></div>
 	<div class="col-md-4"><strong><a href="" class="sort" data-sort=".memberField">Name</a></strong></div>
@@ -468,7 +468,7 @@ get_header(); ?>
 	<div class="col-md-2"></div>
 </div>
 <div id="values">
-<?php 
+<?php
 	$NRmembers = count($clanMembers);
 	$counter = 0;
 	foreach ($clanMembers as $key => $member) {
@@ -476,7 +476,7 @@ get_header(); ?>
 		$counter++;
 		if($counter == $NRmembers){
 			$extraClass = '_last';
-			
+
 		}
 		$member_data = get_userdata($member);
 		$networth = get_user_meta($member, 'networth',true);
@@ -490,9 +490,9 @@ get_header(); ?>
 <div class="row clan_profile_row">
 	<div class="col-md-1">
 		<?php echo small_avatar($member,'');?>
-		
-		
-		
+
+
+
 	</div>
 	<div class="col-md-4 clan_column center_clan_col border_bottom_mobile">
 		<div class="ctclField">
@@ -504,17 +504,17 @@ get_header(); ?>
 			} ?>
 			</div>
 		<a class="memberField <?php echo get_user_meta($member,'status',true);?>" href="/users/profile/?id=<?php echo $member;?>">
-			
-			<?php echo $member_data->display_name.' (#'.$member.')';?></a> 
+
+			<?php echo $member_data->display_name.' (#'.$member.')';?></a>
 			<?php if(!empty($last_online)){
 					if($last_seen < 7200 && !empty($last_online[0])){
 						echo ' <span style="color:#ff0000">*</span>';
 						}
 					}?>
-					
-		
-			
-			
+
+
+
+
 	</div>
 	<div class="col-md-2 clan_column border_bottom_mobile">
 		<span class="clan_data_left">Networth</span>
@@ -535,7 +535,7 @@ get_header(); ?>
 		<?php echo number_format($pts, 0, ',', ' '); ?>pts
 		</span>
 	</div>
-	
+
 	<div class="col-md-1 clan_column center_clan_col">
 		<?php if($userId == $clanleader && $member != $userId){?>
 			<a href="/kick.php/?id=<?php echo $member;?>&clan=<?php echo $clan_id;?>" onclick="return confirm('Are you sure you want to kick <?php echo $member_data->display_name.' (#'.$member.')';?> from your clan? Your clan will lose <?php echo round($pts*0.25);?> clan points.')">Kick</a>
@@ -582,11 +582,11 @@ get_header(); ?>
 	<div class="notice_message"><span class="rdw-line">The round has ended!</span></div>
 		<?php else:?>
 
-	
+
 		<?php if(array_key_exists($clan_id, $cooldownlist)):?>
 
 		<div class="notice_message"><span class="rdw-line">
-			<?php 
+			<?php
 				$timeleft = $cooldownlist[$clan_id]-$timestamp;
 				echo human_time_diff( $cooldownlist[$clan_id],$timestamp);?> left before you can declare a new war on this clan</span>
 				<?php if($warcount == 1 && in_array($declarer_ID, $allowed_to_declare)):?>
@@ -598,15 +598,15 @@ get_header(); ?>
 				<?php endif;?>
 		</div>
 		<?php endif;?>
-		
-		
+
+
 
 
 <?php if (in_array($clan_id, $declared_on)){ ?>
 	<div class="notice_message"><span class="rdw-line">You are at war with this clan</span></div>
 <?php }?><br/>
 		<?php endif;?>
-		
+
 		</div><!-- /.span12 -->
 
 	</div><!-- /row -->
