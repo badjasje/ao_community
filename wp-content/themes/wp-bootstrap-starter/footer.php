fcee17cf3776643867512f11a1826711818c8c28
De Webmakers (Jaap) <JBroeders@dewebmakers.local>
2019-02-11 13:42:16 +0100

    No php needed here

--- wp-content/themes/wp-bootstrap-starter/footer.php
+++ wp-content/themes/wp-bootstrap-starter/footer.php
@@ -7,18 +7,18 @@
 		<div class="container-fluid p-3 p-md-5">
             <div class="site-info">
 	            <center>
-                &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
-                <span class="sep"> | </span>
-                Your local date/time is <strong><?php echo '<script type="text/javascript">
-                var x = new Date().toString();
-                y = x.substr(1,4)+"-";
-                document.write(x);
-                </script>';?></strong>.
-                <br/>Current server date/time is <strong><?php echo date("d-m-Y | G:i", strtotime('+1 hours')); ?></strong><br/>
-                <?php if(is_user_logged_in()):?>
-                <a href="<?php echo wp_logout_url( get_site_url()."/home/" ); ?>">Logout</a></center>
-                <?php endif;?>
-
+                    &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
+                    <span class="sep"> | </span>
+                    Your local date/time is <strong><script type="text/javascript">
+                    var x = new Date().toString();
+                    y = x.substr(1,4)+"-";
+                    document.write(x);
+                    </script></strong>.
+                    <br/>Current server date/time is <strong><?php echo date("d-m-Y | G:i", strtotime('+1 hours')); ?></strong><br/>
+                    <?php if(is_user_logged_in()):?>
+                        <a href="<?php echo wp_logout_url( get_site_url()."/home/" ); ?>">Logout</a>
+                    <?php endif;?>
+                </center>
             </div><!-- close .site-info -->
 		</div>
 	</footer><!-- #colophon -->
