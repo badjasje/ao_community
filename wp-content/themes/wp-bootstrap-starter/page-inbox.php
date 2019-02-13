92c39dfffa4b16db7b6f49a1ee77e5f9b28fe75f
Jaap Broeders <info@jaapbroeders.com>
2019-02-13 09:22:53 +0100

    Fix php error in inbox

--- wp-content/themes/wp-bootstrap-starter/page-inbox.php
+++ wp-content/themes/wp-bootstrap-starter/page-inbox.php
@@ -2,15 +2,15 @@
  /*
  * Template Name: Conversations
 */
-get_header(); 
+get_header();
 global $userId;
 global $userData;
 $backColor = "45, 67, 81";
 update_user_meta($userId,'new_messages',0);
 ?>
 
-<div class="row pageRow">	
-	
+<div class="row pageRow">
+
 <div class="row headerRow fw-row row-no-padding" style="border-bottom:1px solid #fff;background-color: rgba(<?php echo $backColor;?>, 0.75);border-top:1px solid #fff;">
 	<div class="col-md-1 celBlock"></div>
 	<div class="col-md-4 celBlock"></div>
@@ -19,9 +19,9 @@ update_user_meta($userId,'new_messages',0);
 	<div class="col-md-1 celBlock"></div>
 </div>
 
-<?php 
+<?php
 $custom_query_args['paged'] = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-	
+
 	$inboxargs = array(
 	'posts_per_page'   => 200,
 	'post_type'		=> 'user_message',
@@ -42,8 +42,8 @@ $custom_query_args['paged'] = get_query_var( 'paged' ) ? get_query_var( 'paged' 
 		),
 	),
 );
-	
-	
+
+
 $posts = get_posts($inboxargs);
 
 // Instantiate custom query
@@ -58,18 +58,18 @@ $wp_query   = $custom_query;
 if ( $custom_query->have_posts() ) :
 	while ( $custom_query->have_posts() ) :
 	$custom_query->the_post();
-$messageId = get_the_id(); 
+$messageId = get_the_id();
 $sender = get_userdata( get_the_author_meta('ID') );
 
 $messageData = get_post_meta($messageId);
 
 $parent_ID = $messageData['parent_message_id'][0];
 
-$receiver_id = $messageData['receiver_id'][0];	
+$receiver_id = $messageData['receiver_id'][0];
 $receiver = get_userdata( $receiver_id );
 $convoWith = $receiver_id;
-if($receiver_id == $userId){	
-	$convoWith = $messageData['sender_id'][0];	
+if($receiver_id == $userId){
+	$convoWith = $messageData['sender_id'][0];
 }
 ?>
 
@@ -77,23 +77,23 @@ if($receiver_id == $userId){
 		<div class="col-md-1 col-no-padding sea_heading allUsersAvatarCol">
 			<?php echo small_avatar($convoWith,'allUsersAvatar');?><span class="mobileUserName"><?php echo get_user_name($convoWith);?></span>
 		</div>
-	
+
 	<div class="col-md-4 celBlock allUsersNameCol">
 
-		<?php echo get_user_name($convoWith);?>		
+		<?php echo get_user_name($convoWith);?>
 
 	</div>
 	<div class="col-md-4 celBlock">
 		<span class="columnDataLeft">Subject</span>
 		<span class="columnDataRight store-pop-span2">
-		
-			<a href="<?php echo get_the_permalink($messageId);?>/#lastrow"> <?php 
-					
+
+			<a href="<?php echo get_the_permalink($messageId);?>/#lastrow"> <?php
+
 			if (strlen(get_the_title($messageId)) > 55) {
 			echo substr(get_the_title($messageId), 0, 55) . '...'; } else {
 			echo get_the_title($messageId);
 			}?></a>
-					
+
 		</span>
 
 	</div>
@@ -103,18 +103,20 @@ if($receiver_id == $userId){
 			<?php echo date('H:i | d-m-Y', $messageData['last_update_stamp'][0]);?>
 		</span>
 	</div>
-	
+
 	<div class="col-md-1 celBlock">
 		<?php
 			$repeater = get_field('sub_messages_rep',$messageId);
-			$last_row = end($repeater);		
-			if($last_row['sender_id_rep'] != $userId){
-				if($messageData['general_status'][0] != 'Read'){
-					echo '<span style="color:#ff0000;">New messages</span>';
+			if(is_array($repeater)) {
+				$last_row = end($repeater);
+				if($last_row['sender_id_rep'] != $userId){
+					if($messageData['general_status'][0] != 'Read'){
+						echo '<span style="color:#ff0000;">New messages</span>';
+					}
 				}
 			}
 		?>
-	
+
 	</div>
 </div> <! // Close profile row -->
 
