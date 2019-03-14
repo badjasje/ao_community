f5f655184f66f63466068686d16610b13a6d38c8
Jaap Broeders <info@jaapbroeders.com>
2019-03-14 10:17:53 +0100

    Input validation

--- attack2.php
+++ attack2.php
@@ -14,7 +14,7 @@ if ('POST' != $_SERVER['REQUEST_METHOD']) {
 
 require(dirname(__FILE__) . '/wp-load.php');
 
-if (! defined('ABSPATH') || get_field('game_status', 'option') != 'Live') {
+if (!defined('ABSPATH') || get_field('game_status', 'option') != 'Live') {
     $array['status'] = 'The round has ended';
     $array['next'] = false;
     echo json_encode($array);
@@ -34,43 +34,35 @@ $arraydif = array_diff_key($entireArray,$units);
 
 $removeKeys = array_keys($arraydif);
 
-
 foreach($removeKeys as $key) {
    unset($entireArray[$key]);
 }
 
 $attackArray = $entireArray;
-if(array_key_exists('tomahawk', $_POST)){
-	$attackArray['tomahawk'] = $_POST['tomahawk'];
+if(array_key_exists('tomahawk', $_POST)) {
+	$attackArray['tomahawk'] = intval($_POST['tomahawk']);
 }
+
 if (get_field('game_status', 'option') == 'Live') {
-    if (! defined('ABSPATH')) {
+    if (!defined('ABSPATH')) {
         exit;
     }
     include('attack_functions.php');
 
-	
-    
-    $attack_nw = $userData['networth'][0];
-    $attack_clan_id = $userData['clan_id_user'][0];
+    $attack_nw = (!empty($userData['networth'][0]) ? $userData['networth'][0] : 0);
+    $attack_clan_id = (!empty($userData['clan_id_user'][0]) ? $userData['clan_id_user'][0] : 0);
 
-    $target_id = $_POST['target_id'];
-    /*if(intval($target_id) <= 10){
-        $array['status'] = 'Cannot attack an administrator';
-		$array['next'] = false;
-		echo json_encode($array);
-		exit;
-	    
-    }*/
+    $target_id = filter_input(INPUT_POST, 'target_id', FILTER_VALIDATE_INT);
+
     $defend_nw = get_user_meta($target_id, 'networth')[0];
     $defend_clan_id = get_user_meta($target_id, 'clan_id_user')[0];
 
-    $attack_type = $_POST['attacktype'];
+    $attack_type = filter_input(INPUT_POST, 'attacktype', FILTER_SANITIZE_STRING);
 
-/* determine war type */
+    /* determine war type */
     $war_type = get_war_type($attack_clan_id, $defend_clan_id);
 
-/* check if target in range */
+    /* check if target in range */
     $in_range = target_in_range($attack_type, $attack_nw, $defend_nw, $war_type);
 
     if (!$in_range) {
@@ -79,46 +71,45 @@ if (get_field('game_status', 'option') == 'Live') {
 		echo json_encode($array);
 		exit;
     }
-    
-if($_POST['attacktype'] != 'missile'){
-$finalAttackArray = array();
-	foreach ($attackArray as $key => $attacking) {
-		
-		$unitsOwned = $userData[$key.'_owned'][0];
-		
-		if($attacking >= $unitsOwned){
-			$percentage = 1;
-		}else{
-			$percentage = $attacking/$unitsOwned;
-		}
-		if($attacking > 0){
-			$finalAttackArray[$key] = $percentage;
-		}
-	}
-	
-	
-$array['attackarray'] = $finalAttackArray;
-}
-if($_POST['attacktype'] == 'missile'){
-	$array['missiletype'] = $_POST['missiletype'];
-}
-if($_POST['attacktype'] == 'spy'){
-	$array['spytype'] = $_POST['spytype'];
-}
-if($_POST['attacktype'] == 'thief'){
-	$array['nothiefs'] = $_POST['nothiefs'];
-}
-if($_POST['attacktype'] == 'satellite'){
-	$array['satellitetype'] = $_POST['satellitetype'];
-}
 
-$array['attacktype'] = $_POST['attacktype'];
-$array['target_id'] = $target_id;
-$array['attackmode'] = $_POST['attackmode'];
-$array['maintarget'] = $_POST['maintarget'];
+    if($_POST['attacktype'] != 'missile') {
+        $finalAttackArray = array();
+	    foreach ($attackArray as $key => $attacking) {
+			$unitsOwned = (!empty($userData[$key.'_owned'][0]) ? intval($userData[$key.'_owned'][0]) : 0);
 
+            if($attacking >= $unitsOwned) {
+                $percentage = 1;
+            }else{
+                $percentage = intval($attacking)/$unitsOwned;
+            }
+            if($attacking > 0) {
+                $finalAttackArray[$key] = $percentage;
+            }
+        }
+        $array['attackarray'] = $finalAttackArray;
+    }
 
-echo json_encode($array);
-exit;
+    if($_POST['attacktype'] == 'missile') {
+        $array['missiletype'] = $_POST['missiletype'];
+    }
 
-}// End live check
+    if($_POST['attacktype'] == 'spy') {
+        $array['spytype'] = $_POST['spytype'];
+    }
+
+    if($_POST['attacktype'] == 'thief') {
+        $array['nothiefs'] = $_POST['nothiefs'];
+    }
+
+    if($_POST['attacktype'] == 'satellite') {
+        $array['satellitetype'] = $_POST['satellitetype'];
+    }
+
+    $array['attacktype'] = $_POST['attacktype'];
+    $array['target_id'] = $target_id;
+    $array['attackmode'] = $_POST['attackmode'];
+    $array['maintarget'] = $_POST['maintarget'];
+
+    echo json_encode($array);
+    exit;
+} // End live check
