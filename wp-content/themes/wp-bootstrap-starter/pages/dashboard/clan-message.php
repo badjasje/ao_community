d7bc0ac7338ac28866bb06e68b6ec95c93906fd9
Jaap Broeders <info@jaapbroeders.com>
2019-03-19 16:20:29 +0100

    Codestyle

--- wp-content/themes/wp-bootstrap-starter/pages/dashboard/clan-message.php
+++ wp-content/themes/wp-bootstrap-starter/pages/dashboard/clan-message.php
@@ -1,122 +1,95 @@
 <div class="blockHeader">Clan message <?php echo get_the_title($clanId);?></div>
-	
- 	<div class="col-md-10 clanMessage">
-	 	<div id="savedmsg">
-	 		<?php echo wpautop($clanMessage);?>
-	 	</div>
-	 	<?php if(in_array($userId, $allowed)):?>
-	 	<div class="message-editor">
-	 		<form class="form" name="new_message" id="clan_message" method="post">
-	 			<?php wp_editor($clanMessage,'new_message',$settings);?>
-	 			<input class="mainSubmit" type="submit" value="Submit" style="float:left;width:50%">
-	 		</form>
-	 		<div id="dismiss" class="mainSubmit" style="background-color:#ddd;color:#000;width:50%;float:left;">Dismiss</div>
-	 	</div>
-	 	<?php endif;?>
- 	</div>
- 	<div class="col-md-2 clanMessageRow">
-	 	<a href="<?php echo get_the_permalink($clanId);?>">
-		 	<div class="col-md-12 clanMessageButton hoverEffect">
-		 		<i class="fa fa-info-circle"></i> View clan
-	 		</div>
-	 	</a>
-	 	
-	 	<a href="<?php echo get_the_permalink(50302);?>">
-	 		<div class="col-md-12 clanMessageButton secondButton hoverEffect">
-		 		<i class="fa fa-users"></i> Members
-	 		</div>
-	 	</a>
-	 	
-	 	<a href="<?php echo get_the_permalink(3842);?>">
-	 		<div class="col-md-12 clanMessageButton hoverEffect">
-		 		<i class="fa fa-fire"></i> Clan wars
-	 		</div>
-	 	</a>
-	 	<a href="<?php echo get_the_permalink(49609);?>">
-	 		<div class="col-md-12 clanMessageButton fourthbutton hoverEffect">
-		 		<i class="fas fa-dollar-sign"></i> Send aid
-	 		</div>
-	 	</a>
- 	</div>
- 	
+
+<div class="col-md-10 clanMessage">
+	<div id="savedmsg">
+		<?php echo wpautop($clanMessage);?>
+	</div>
+	<?php if(in_array($userId, $allowed)):?>
+	<div class="message-editor">
+		<form class="form" name="new_message" id="clan_message" method="post">
+			<?php wp_editor($clanMessage,'new_message',$settings);?>
+			<input class="mainSubmit" type="submit" value="Submit" style="float:left;width:50%">
+		</form>
+		<div id="dismiss" class="mainSubmit" style="background-color:#ddd;color:#000;width:50%;float:left;">Dismiss</div>
+	</div>
+	<?php endif;?>
+</div>
+
+<div class="col-md-2 clanMessageRow">
+	<a href="<?php echo get_the_permalink($clanId);?>">
+		<div class="col-md-12 clanMessageButton hoverEffect">
+			<i class="fa fa-info-circle"></i> View clan
+		</div>
+	</a>
+
+	<a href="<?php echo get_the_permalink(50302);?>">
+		<div class="col-md-12 clanMessageButton secondButton hoverEffect">
+			<i class="fa fa-users"></i> Members
+		</div>
+	</a>
+	<? if(get_field('game_status','option')=='Pause') { ?>
+		<div class="col-md-12 clanMessageButton disabled">
+			<i class="fa fa-fire"></i> Clan wars
+		</div>
+		<div class="col-md-12 clanMessageButton fourthbutton disabled">
+			<i class="fas fa-dollar-sign"></i> Send aid
+		</div>
+	<? } else { ?>
+		<a href="<?php echo get_the_permalink(3842);?>">
+			<div class="col-md-12 clanMessageButton hoverEffect">
+				<i class="fa fa-fire"></i> Clan wars
+			</div>
+		</a>
+		<a href="<?php echo get_the_permalink(49609);?>">
+			<div class="col-md-12 clanMessageButton fourthbutton hoverEffect">
+				<i class="fas fa-dollar-sign"></i> Send aid
+			</div>
+		</a>
+	<? } ?>
+
+</div>
+
 <?php if(in_array($userId, $allowed)):?>
 	<div class="mainSubmit" id="editmessage"><i class="fas fa-pencil-alt"></i> Edit clan message</div>
 <?php endif;?>
 
-
 <script>
-	jQuery( "#editmessage" ).click(function() {
-		jQuery('.message-editor').show(750);
-		jQuery('#savedmsg').hide(750);
-		
-	});
-	jQuery( "#dismiss" ).click(function() {
-		jQuery('.message-editor').hide(750);
-		jQuery('#savedmsg').show(750);
-	});
-	
-(function($) {
-	
+	(function($) {
+		$( "#editmessage" ).click(function() {
+			$('.message-editor').show(750);
+			$('#savedmsg').hide(750);
+		});
+		$( "#dismiss" ).click(function() {
+			$('.message-editor').hide(750);
+			$('#savedmsg').show(750);
+		});
 
-// Variable to hold request
-var request;
+		// Variable to hold request
+		var request;
 
-// Bind to the submit event of our form
-$("#clan_message").submit(function(event){
-	var content = tinymce.activeEditor.getContent();
-	$('.pageLoader, #page-cover').show();
-	$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
-    // Prevent default posting of form - put here to work in case of errors
-    event.preventDefault();
+		// Bind to the submit event of our form
+		$("#clan_message").on('submit',function(event){
+			var content = tinymce.activeEditor.getContent();
+			$('.pageLoader, #page-cover').show();
+			$('.pageLoader, #page-cover').delay(250).fadeOut("fast");
 
-    // Abort any pending request
-    if (request) {
-        request.abort();
-    }
-    // setup some local variables
-    var $form = $(this);
+			event.preventDefault();
+			if (request) request.abort();
 
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
+			var $form = $(this);
+			var $inputs = $form.find("input, select, button, textarea");
+			var serializedData = $form.serialize();
 
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    //$inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "/clan_message.php",
-        type: "post",
-        data: serializedData+'&new_message='+content
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        var array = JSON.parse(response);
-        	
-        						
-				$.notify({
-					message: array.status,
-					},{
-					type: 'info',
-					delay: 5000,
-					allow_dismiss: true,
-					newest_on_top: true,
-						});	
-			
-			$('#savedmsg').html(array.clanmessage);
-			jQuery('.message-editor').hide(400);
-			jQuery('#savedmsg').show(500);
-			
-		
-});	});	
-})(jQuery);
-
-	
-	
+			// Fire off the request to /form.php
+			request = $.ajax({url: "/clan_message.php",type: "post",data: serializedData+'&new_message='+content});
+			request.done(function (response, textStatus, jqXHR){
+				var array = JSON.parse(response);
+				$.notify({message: array.status},{type: 'info',delay: 5000,allow_dismiss: true,newest_on_top: true});
+				$('#savedmsg').html(array.clanmessage);
+				$('.message-editor').hide(400);
+				$('#savedmsg').show(500);
+			});
+		});
+	})(jQuery);
 </script>
 
--- wp-content/themes/wp-bootstrap-starter/pages/view-clan/member.php
+++ wp-content/themes/wp-bootstrap-starter/pages/view-clan/member.php
@@ -5,99 +5,77 @@
 		</a>
 	</div>
 	<?php if(in_array($userId, $allowed)):?>
-	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-		<a href="/edit-clan">
-			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.95)"><i class="fa fa-wrench"></i> Edit clan</button>
-		</a>
-	</div>
-	<?php if($userId == $clan_leader):?>
-	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-		<a href="/clan-wars-opt-in-or-out">
-			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.95)"><i class="fas fa-check"></i> Opt In or Out of Clan Wars</button>
-		</a>
-	</div>
+		<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
+			<a href="/edit-clan">
+				<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.95)"><i class="fa fa-wrench"></i> Edit clan</button>
+			</a>
+		</div>
+			<?php if($userId == $clan_leader):?>
+			<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
+				<a href="/clan-wars-opt-in-or-out">
+					<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.95)"><i class="fas fa-check"></i> Opt In or Out of Clan Wars</button>
+				</a>
+			</div>
+			<?php endif;?>
+		<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
+			<a href="/open-invites">
+				<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.9)"><i class="fa fa-envelope-open"></i> Open invites</button>
+			</a>
+		</div>
 	<?php endif;?>
+
 	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-		<a href="/open-invites">
-			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.9)"><i class="fa fa-envelope-open"></i> Open invites</button>
-		</a>
-	</div>
-	<?php endif;?>
-	
-	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-			<a href="/clan-member-information">
+		<a href="/clan-member-information">
 			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.85)"><i class="fa fa-users"></i> Clan member information</button>
-			</a>
+		</a>
 	</div>
 	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-			<a href="/clan-wars">
+		<a href="/clan-wars">
 			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.80)"><i class="fa fa-fire"></i> Clan wars</button>
-			</a>
+		</a>
 	</div>
-	
+
 	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-			<a href="/bonus-overview">
+		<a href="/bonus-overview">
 			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.75)"><i class="fa fa-chart-bar"></i> Bonus overview</button>
-			</a>
+		</a>
 	</div>
 	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-			<a href="/send-aid">
+		<a href="/send-aid">
 			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.70)"><i class="fa fa-dollar-sign"></i> Send aid</button>
-			</a>
+		</a>
 	</div>
 	<?php if($clan_leader != $userId):?>
 	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-			<a onclick="return confirm('Are you sure you want to leave your clan? Your clan will lose 25% of your total clan points.')" href="/leave.php/?user=<?php echo $userId;?>">
+		<a onclick="return confirm('Are you sure you want to leave your clan? Your clan will lose 25% of your total clan points.')" href="/leave.php/?user=<?php echo $userId;?>">
 			<button class="cancelButton hoverEffect" style="background-color:rgba(66, 92, 107,0.65)"><i class="fa fa-arrow-circle-o-down"></i> Leave clan</button>
-			</a>
+		</a>
 	</div>
 	<?php endif;?>
-	
+
 	<?php if($clan_leader == $userId):?>
-	<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
-		<button onclick="return confirm('Are you sure you want to delete your clan?')" class="cancelButton deleteClan hoverEffect" style="background-color:rgba(66, 92, 107,0.65)"><i class="fa fa-trash"></i> Delete clan</button>
-	</div>
-	
-	
-<script>
-(function($) {
-	var deleteclan;
-	
-	$(document).on('click','.deleteClan',function(){
-	
-	$('.pageLoader, #page-cover').show();
-	$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
-		deleteclan = $.ajax({
-			url: "/deleteclan.php",
-			type: "post",
-			data: '&clan=<?php echo $clan_id_user;?>'
-		});
-						
+		<div class="col-md-6 col-lg-4 celBlock" style="padding:0px;">
+			<button onclick="return confirm('Are you sure you want to delete your clan?')" class="cancelButton deleteClan hoverEffect" style="background-color:rgba(66, 92, 107,0.65)"><i class="fa fa-trash"></i> Delete clan</button>
+		</div>
+		<script>
+		(function($) {
+			var deleteclan;
 
-		deleteclan.done(function (response, textStatus, jqXHR){
+			$(document).on('click','.deleteClan',function(){
 
-				var response = $.parseJSON(response);
-				$.notify({
-					message: response.status,
-					},{
-					type: 'info',
-					delay: 5000,
-					allow_dismiss: true,
-					newest_on_top: true,
-			});	
-			if(response.next == true){
-				location.reload();
-			}
-		});
-				
-	});
+			$('.pageLoader, #page-cover').show();
+			$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
+				deleteclan = $.ajax({url: "/deleteclan.php",type: "post",data: '&clan=<?php echo $clan_id_user;?>'});
+				deleteclan.done(function (response, textStatus, jqXHR){
+					var response = $.parseJSON(response);
+					$.notify({message: response.status},{type: 'info',delay: 5000,allow_dismiss: true,newest_on_top: true});
+					if(response.next == true){
+						location.reload();
+					}
+				});
+			});
 
-})(jQuery);
-</script>
-	
-	
+		})(jQuery);
+		</script>
 	<?php endif;?>
-		
-		
-	
-	</div>
+</div>
