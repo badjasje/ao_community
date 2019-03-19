bdfa8666c6625dd3272632483abe4d8ad718b23a
Jaap Broeders <info@jaapbroeders.com>
2019-03-19 16:54:09 +0100

    Disable some buttons during round break

--- wp-content/themes/wp-bootstrap-starter/page-profile.php
+++ wp-content/themes/wp-bootstrap-starter/page-profile.php
@@ -47,7 +47,7 @@ $clan_id_user = $visitorData['clan_id_user'][0];
 $visitorClanData = get_post_meta($clan_id_user);
 
 $previous_members = maybe_unserialize(get_post_meta($clan_id_user, 'previous_members', true));
-
+if(!is_array($previous_members)) $previous_members = array();
 
 $ct_1 = $visitorClanData['ct_1'][0];
 $ct_2 = $visitorClanData['ct_2'][0];
@@ -57,6 +57,9 @@ $cl_1 = $visitorClanData['clan_leader'][0];
 
 $CT_CL_array = array($ct_1,$ct_2,$ct_3,$ct_4,$cl_1);
 $members = $visitorClanData['clan_members'][0];
+if(!is_array($members)) $members = array();
+
+$game_live = (get_field('game_status','option')=='Live');
 ?>
 
 <div class="row pageRow">
@@ -140,11 +143,13 @@ $members = $visitorClanData['clan_members'][0];
 
 <!-- Visiting non-clanmember as non CT/CL -->
 <div class="row fw-row no-gutters profileButtonRow">
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/attack/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/attack/?id='.$viewedId)?>">
 		<i class="fa fa-crosshairs" aria-hidden="true"></i> &nbsp;Attack
 	</a>
 
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.9);"href="/spy-reports/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 0.9);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/spy-reports/?id='.$viewedId)?>">
  		<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;Spy reports
  	</a>
 
@@ -152,19 +157,16 @@ $members = $visitorClanData['clan_members'][0];
  		<i class="fas fa-envelope" aria-hidden="true"></i> &nbsp;Send message
 	</a>
 
-<?php if(in_array($viewedId, $savedUsers)):?>
+	<?php if(in_array($viewedId, $savedUsers)):?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
+			<i class="fas fa-save"></i> &nbsp;User saved
+		</a>
+	<?php else:?>
 
-	<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
-		<i class="fas fa-save"></i> &nbsp;User saved
-	</a>
-
-<?php else:?>
-
-	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
-		<i class="fas fa-save"></i> &nbsp;Save user
-	</a>
-
-<?php endif;?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
+			<i class="fas fa-save"></i> &nbsp;Save user
+		</a>
+	<?php endif;?>
 
 </div>
 <?php endif;?>
@@ -173,11 +175,13 @@ $members = $visitorClanData['clan_members'][0];
 <?php $count = 1;?>
 <!-- Visiting non-clanmember as non CT/CL -->
 <div class="row fw-row no-gutters profileButtonRow">
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/attack/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/attack/?id='.$viewedId)?>">
 		<i class="fa fa-crosshairs" aria-hidden="true"></i> &nbsp;Attack
 	</a>
 
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.9);"href="/spy-reports/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 0.9);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/spy-reports/?id='.$viewedId)?>">
  		<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;Spy reports
  	</a>
 
@@ -185,19 +189,15 @@ $members = $visitorClanData['clan_members'][0];
  		<i class="fas fa-envelope" aria-hidden="true"></i> &nbsp;Send message
 	</a>
 
-<?php if(in_array($viewedId, $savedUsers)):?>
-
-	<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
-		<i class="fas fa-save"></i> &nbsp;User saved
-	</a>
-
-<?php else:?>
-
-	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
-		<i class="fas fa-save"></i> &nbsp;Save user
-	</a>
-
-<?php endif;?>
+	<?php if(in_array($viewedId, $savedUsers)):?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
+			<i class="fas fa-save"></i> &nbsp;User saved
+		</a>
+	<?php else:?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
+			<i class="fas fa-save"></i> &nbsp;Save user
+		</a>
+	<?php endif;?>
 
 </div>
 <?php endif;?>
@@ -207,11 +207,13 @@ $members = $visitorClanData['clan_members'][0];
 <?php $count = 1;?>
 
 <div class="row no-gutters fw-row profileButtonRow">
-	<a class="col-md-2 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/attack/?id=<?php echo $viewedId;?>">
+	<a class="col-md-2 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+		href="<?=(!$game_live?'javascript:void(0);':'/attack/?id='.$viewedId)?>">
 		<i class="fa fa-crosshairs" aria-hidden="true"></i> &nbsp;Attack
 	</a>
 
- 	<a class="col-md-2 profileButton" style="background-color: rgba(70, 118, 94, 0.9);"href="/spy-reports/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-2 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 0.9);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/spy-reports/?id='.$viewedId)?>">
  		<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;Spy reports
  	</a>
 
@@ -219,47 +221,42 @@ $members = $visitorClanData['clan_members'][0];
  		<i class="fas fa-envelope" aria-hidden="true"></i> &nbsp;Send message
 	</a>
 
-<?php if(in_array($viewedId, $previous_members)):?>
+	<?php if(in_array($viewedId, $previous_members)):?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="#">
+			<i class="fa fa-user-plus" aria-hidden="true"></i> &nbsp;Cannot invite
+		</a>
+	<?php else:?>
+		<a 	style="background-color: rgba(70, 118, 94, 0.7);"
+			class="col-md-3 profileButton"
+			onclick="return confirm('Are you sure you want to invite <?php echo $user->display_name;?> (#<?php echo $viewedId;?>)?')"
+			href="/invite.php?invite=<?php echo md5(uniqid(rand(), TRUE)) . "\n";?>&clan=<?php echo $clan_id_user;?>&user=<?php echo $viewedId;?>">
+					<i class="fa fa-user-plus" aria-hidden="true"></i> &nbsp;Send clan invite
+		</a>
+	<?php endif;?>
 
-	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="#">
-		<i class="fa fa-user-plus" aria-hidden="true"></i> &nbsp;Cannot invite
-	</a>
+	<?php if(in_array($viewedId, $savedUsers)):?>
+		<a class="col-md-2 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
+			<i class="fas fa-save"></i> &nbsp;User saved
+		</a>
+	<?php else:?>
+		<a class="col-md-2 profileButton" style="background-color: rgba(70, 118, 94, 0.6);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
+			<i class="fas fa-save"></i> &nbsp;Save user
+		</a>
+	<?php endif;?>
 
-<?php else:?>
-
-	<a 	style="background-color: rgba(70, 118, 94, 0.7);"
-		class="col-md-3 profileButton"
-		onclick="return confirm('Are you sure you want to invite <?php echo $user->display_name;?> (#<?php echo $viewedId;?>)?')"
-		href="/invite.php?invite=<?php echo md5(uniqid(rand(), TRUE)) . "\n";?>&clan=<?php echo $clan_id_user;?>&user=<?php echo $viewedId;?>">
-			  	<i class="fa fa-user-plus" aria-hidden="true"></i> &nbsp;Send clan invite
-	</a>
-
-<?php endif;?>
-
-<?php if(in_array($viewedId, $savedUsers)):?>
-
-	<a class="col-md-2 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
-		<i class="fas fa-save"></i> &nbsp;User saved
-	</a>
-
-<?php else:?>
-
-	<a class="col-md-2 profileButton" style="background-color: rgba(70, 118, 94, 0.6);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
-		<i class="fas fa-save"></i> &nbsp;Save user
-	</a>
-
-<?php endif;?>
 </div>
 <?php endif;?>
 
 <?php if($visiting_user != $viewedId && $clan_id != $clan_id_user && $count != 1 && in_array($visiting_user, $CT_CL_array)):?>
 <!-- Visiting non-clanmember as non CT/CL -->
 <div class="row fw-row no-gutters profileButtonRow">
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/attack/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/attack/?id='.$viewedId)?>">
 		<i class="fa fa-crosshairs" aria-hidden="true"></i> &nbsp;Attack
 	</a>
 
- 	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.9);"href="/spy-reports/?id=<?php echo $viewedId;?>">
+ 	<a class="col-md-3 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 0.9);"
+	 	href="<?=(!$game_live?'javascript:void(0);':'/spy-reports/?id='.$viewedId)?>">
  		<i class="fa fa-binoculars" aria-hidden="true"></i> &nbsp;Spy reports
  	</a>
 
@@ -267,19 +264,15 @@ $members = $visitorClanData['clan_members'][0];
  		<i class="fas fa-envelope" aria-hidden="true"></i> &nbsp;Send message
 	</a>
 
-<?php if(in_array($viewedId, $savedUsers)):?>
-
-	<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
-		<i class="fas fa-save"></i> &nbsp;User saved
-	</a>
-
-<?php else:?>
-
-	<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
-		<i class="fas fa-save"></i> &nbsp;Save user
-	</a>
-
-<?php endif;?>
+	<?php if(in_array($viewedId, $savedUsers)):?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
+			<i class="fas fa-save"></i> &nbsp;User saved
+		</a>
+	<?php else:?>
+		<a class="col-md-3 profileButton" style="background-color: rgba(70, 118, 94, 0.7);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
+			<i class="fas fa-save"></i> &nbsp;Save user
+		</a>
+	<?php endif;?>
 
 </div>
 <?php endif;?>
@@ -288,7 +281,8 @@ $members = $visitorClanData['clan_members'][0];
 <!-- Visiting clanmember profile -->
 <div class="row fw-row no-gutters profileButtonRow">
 
-	<a class="col-md-4 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/military-overview/?id=<?php echo $viewedId;?>">
+	<a class="col-md-4 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+		href="<?=(!$game_live?'javascript:void(0);':'/military-overview/?id='.$viewedId)?>">
 		<i class="fa fa-bars" aria-hidden="true"></i> &nbsp;Military overview
 	</a>
 
@@ -296,28 +290,24 @@ $members = $visitorClanData['clan_members'][0];
 		<i class="fas fa-envelope" aria-hidden="true"></i> &nbsp;Send message
 	</a>
 
-<?php if(in_array($viewedId, $savedUsers)):?>
-
-	<a class="col-md-4 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
-		<i class="fas fa-save"></i> &nbsp;User saved
-	</a>
-
-<?php else:?>
-
-	<a class="col-md-4 profileButton" style="background-color: rgba(70, 118, 94, 0.8);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
-		<i class="fas fa-save"></i> &nbsp;Save user
-	</a>
-
-<?php endif;?>
+	<?php if(in_array($viewedId, $savedUsers)):?>
+		<a class="col-md-4 profileButton" style="background-color: rgba(0, 0, 0, 0.5)" href="/saved-users">
+			<i class="fas fa-save"></i> &nbsp;User saved
+		</a>
+	<?php else:?>
+		<a class="col-md-4 profileButton" style="background-color: rgba(70, 118, 94, 0.8);" href="/save_user.php/?id=<?php echo $viewedId;?>&return=<?php echo get_the_id();?>">
+			<i class="fas fa-save"></i> &nbsp;Save user
+		</a>
+	<?php endif;?>
 
 </div>
-
 <?php endif;?>
 
 <?php if($visiting_user == $viewedId):?>
 <!-- visiting own profile -->
 <div class="row no-gutters fw-row profileButtonRow">
-	<a class="col-md-4 profileButton" style="background-color: rgba(70, 118, 94, 1);" href="/military-overview/?id=<?php echo $viewedId;?>">
+	<a class="col-md-4 profileButton<?=(!$game_live?' disabled':'')?>" style="background-color: rgba(70, 118, 94, 1);"
+		href="<?=(!$game_live?'javascript:void(0);':'/military-overview/?id='.$viewedId)?>">
 		<i class="fa fa-bars" aria-hidden="true"></i> &nbsp;Military overview
 	</a>
 
