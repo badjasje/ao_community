b36a35ab8c85bcfc8cd5598cbcf13750d714d2d7
Jaap Broeders <info@jaapbroeders.com>
2019-04-11 11:39:27 +0200

    Whitespace

--- nw_list_updater.php
+++ nw_list_updater.php
@@ -1,30 +1,27 @@
 <?php
 require_once("wp-load.php");
+
 $timestamp = current_time('timestamp');
-	$args = array(
-
-		'meta_key'     	=> 'last_online',
-		'orderby'      	=> 'meta_value_num',
-		'meta_value'	=> $timestamp-259200,
-		'meta_compare'	=> '>',
-
-	 ); 
+$args = array(
+    'meta_key'     	=> 'last_online',
+    'orderby'      	=> 'meta_value_num',
+    'meta_value'	=> $timestamp-259200,
+    'meta_compare'	=> '>',
+);
 $users = get_users($args);
 foreach ($users as $user) {
     $user_ID = $user->data->ID;
     count_all_stats($user_ID);
 }
-    
+
 $args = array(
-        
-        'post_type'     =>  'clan',
-        'posts_per_page' => -1,
-        );
-    
-    $clans = get_posts($args);
+    'post_type'     =>  'clan',
+    'posts_per_page' => -1,
+);
+$clans = get_posts($args);
 foreach ($clans as $clan) {
     $clan_members = get_post_meta($clan->ID, 'clan_members');
-        
+
     $tot_networth = 0;
     foreach ($clan_members[0] as $member) {
 	    $status = get_user_meta($member, 'status', true);
