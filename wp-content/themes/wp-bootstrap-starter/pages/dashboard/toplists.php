43826d88b7f3afdd1cdabfe50387451b5361e74c
Jaap Broeders <info@jaapbroeders.com>
2019-04-25 13:57:45 +0200

    Fix mobile view

--- wp-content/themes/wp-bootstrap-starter/pages/dashboard/toplists.php
+++ wp-content/themes/wp-bootstrap-starter/pages/dashboard/toplists.php
@@ -9,8 +9,8 @@ if(count($toplistArray) && isset($toplistArray['clannetworth'])) {
                 <div class="blockHeader"><strong>Top clan pts today</strong></div>
                 <? foreach(array_slice($toplistArray['24h_pts'],0,3) as $topClanId) {
                     ?>
-                    <div class="col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
-                    <div class="col-sm-4 celBlock"><?=ceil(get_post_meta($topClanId, '24h_pts',true))?></div>
+                    <div class="col-xs-6 col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
+                    <div class="col-xs-6 col-sm-4 celBlock"><?=ceil(get_post_meta($topClanId, '24h_pts',true))?></div>
                     <?
                 } ?>
             </div>
@@ -20,8 +20,8 @@ if(count($toplistArray) && isset($toplistArray['clannetworth'])) {
                 <div class="blockHeader"><strong>Top clan nw</strong></div>
                 <? foreach(array_slice($toplistArray['clannetworth'],0,3) as $topClanId) {
                     ?>
-                    <div class="col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
-                    <div class="col-sm-4 celBlock">$ <?=number_format(get_post_meta($topClanId, 'clan_networth',true), 0, ',', ' ')?></div>
+                    <div class="col-xs-6 col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
+                    <div class="col-xs-6 col-sm-4 celBlock">$ <?=number_format(get_post_meta($topClanId, 'clan_networth',true), 0, ',', ' ')?></div>
                     <?
                 } ?>
             </div>
@@ -31,8 +31,8 @@ if(count($toplistArray) && isset($toplistArray['clannetworth'])) {
                 <div class="blockHeader"><strong>Top clan pts total</strong></div>
                 <? foreach(array_slice($toplistArray['clanpoints'],0,3) as $topClanId) {
                     ?>
-                    <div class="col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
-                    <div class="col-sm-4 celBlock"><?=get_post_meta($topClanId, 'clan_points',true)?></div>
+                    <div class="col-xs-6 col-sm-8 celBlock"><a href="<?php echo get_the_permalink($topClanId);?>"><?=get_the_title($topClanId)?></a></div>
+                    <div class="col-xs-6 col-sm-4 celBlock"><?=get_post_meta($topClanId, 'clan_points',true)?></div>
                     <?
                 } ?>
             </div>
