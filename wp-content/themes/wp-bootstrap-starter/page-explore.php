45b03e389cbb4456eed2508fb3b1ce530b7c2765
Jaap Broeders <info@jaapbroeders.com>
2019-05-03 09:12:29 +0200

    Codestyle

--- wp-content/themes/wp-bootstrap-starter/page-explore.php
+++ wp-content/themes/wp-bootstrap-starter/page-explore.php
@@ -1,7 +1,7 @@
 <?php
  /*
  * Template Name: Explore
-*/
+ */
 
 get_header();
 global $userData;
@@ -10,185 +10,96 @@ $ownedland = $userData['land'][0];
 $builtLand = $userData['builtland'][0];
 $freeland = $ownedland-$builtLand;
 $activeTab = isset($_GET['tab']) ? sanitize_text_field($_GET['tab']) : 'explore';
-?>
 
+?>
 <div class="row pageRow">
 
-
-<div class="fw-row">
+	<div class="fw-row">
 		<nav class="nav nav-pills nav-fill flex-column flex-sm-row">
 			<a class="nav-item nav-link navItem <?php echo $activeTab === 'explore' ? 'active' : ''; ?>" data-toggle="tab" data-target="#explore" href="?tab=explore">Explore</a>
 			<a class="nav-item nav-link navItem <?php echo $activeTab === 'sell' ? 'active' : ''; ?>" data-toggle="tab" data-target="#sell" href="?tab=sell">Sell</a>
 		</nav>
 	</div>
 
-
-<div class="fw-row">
-	<div class="tab-content current build_content tabbed-table">
-				<div class="tab-pane <?php echo $activeTab === 'explore' ? 'active' : ''; ?>"  id="explore" role="tabpanel">
-
-					<?php include 'pages/explore/explore.php'; ?>
-
-				</div>
-
-
-				<div class="tab-pane <?php echo $activeTab === 'sell' ? 'active' : ''; ?>"  id="sell" role="tabpanel">
-
-					<?php include 'pages/explore/sell.php'; ?>
-
-				</div>
+	<div class="fw-row">
+		<div class="tab-content current build_content tabbed-table">
+			<div class="tab-pane <?php echo $activeTab === 'explore' ? 'active' : ''; ?>"  id="explore" role="tabpanel">
+				<?php include 'pages/explore/explore.php'; ?>
 			</div>
-</div>
+			<div class="tab-pane <?php echo $activeTab === 'sell' ? 'active' : ''; ?>"  id="sell" role="tabpanel">
+				<?php include 'pages/explore/sell.php'; ?>
+			</div>
+		</div>
+	</div>
 
-<?php
-if($userData['turns'][0] > 150 && $userData['money'][0] < 70000) {
-	helpText('Low on money? Use some turns to explore and sell', 'explore', 'reminder');
-}
-if($maxSell > 700) {
-	helpText('You will lose unused land when attacked', 'explore', 'reminder');
-}
-?>
+	<?php
+	if($userData['turns'][0] > 150 && $userData['money'][0] < 70000) {
+		helpText('Low on money? Use some turns to explore and sell', 'explore', 'reminder');
+	}
+	if($maxSell > 700) {
+		helpText('You will lose unused land when attacked', 'explore', 'reminder');
+	}
+	?>
 
-<script>
-(function($) {
+	<script>
+	(function($) {
+		$(".maxexp").on('click',function() {
+			var maxexp = $(this).attr( "data-max" );
+			$("#turnsinput").val(maxexp);
+		});
+		$(".maxsell").on('click',function() {
+			$("#landinput").val("<?php echo $maxSell;?>");
+		});
 
-	$(".maxexp").on('click',function() {
-		var maxexp = $(this).attr( "data-max" );
-		$("#turnsinput").val(maxexp);
-	});
-	$(".maxsell").on('click',function() {
-		$("#landinput").val("<?php echo $maxSell;?>");
-	});
+		var request;
+		$('#exploreform').submit(function( event ) {
+			$('.pageLoader, #page-cover').show();
+			$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
 
-var request;
+			event.preventDefault();
+			if (request) request.abort();
 
-$('#exploreform').submit(function( event ) {
-	$('.pageLoader, #page-cover').show();
-	$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
+			request = $.ajax({url: "/explore.php",type: "post",data: $(this).serialize()});
+			request.done(function (response, textStatus, jqXHR){
+				updateHeaderData();
+				var array = JSON.parse(response);
+				$.notify({message: array.status},{type: 'info', delay: 5000, allow_dismiss: true, newest_on_top: true});
+				if(array.next == true){
+					$( ".explNotice" ).empty();
+					$( ".explNotice" ).append(array.exploredtoday);
+					$('#exprate').html(array.newrate);
 
-    event.preventDefault();
+					$("#turnsinput").attr({"max" : array.maxturns, "min" : 0});
+					$("#maxexp").attr({"data-max" : array.maxturns, "min" : 0});
 
-    if (request) {
-        request.abort();
-    }
+					$('form').trigger("reset");
+					location.reload();
+				}
+			});
+		});
 
-    var $form = $(this);
+		$('#sellform').submit(function( event ) {
+			$('.pageLoader, #page-cover').show();
+			$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
 
-    var $inputs = $form.find("input, select, button, textarea");
+			event.preventDefault();
+			if (request) request.abort();
 
-
-    var serializedData = $form.serialize();
-
-    request = $.ajax({
-        url: "/explore.php",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        updateHeaderData();
-        var array = JSON.parse(response);
-
-
-				$.notify({
-					message: array.status,
-					},{
-					type: 'info',
-					delay: 5000,
-					allow_dismiss: true,
-					newest_on_top: true,
-						});
-
-
-			if(array.next == true){
-
-				$( ".explNotice" ).empty();
-				$( ".explNotice" ).append(array.exploredtoday);
-				$('#exprate').html(array.newrate);
-
-				$("#turnsinput").attr({
-					"max" : array.maxturns,
-					"min" : 0
-				});
-				$("#maxexp").attr({
-					"data-max" : array.maxturns,
-					"min" : 0
-				});
-
-
-
-				$('form').trigger("reset");
-				location.reload();
-			}
-});	});
-
-
-// jQuery Sell
-
-var request;
-
-
-$('#sellform').submit(function( event ) {
-	$('.pageLoader, #page-cover').show();
-	$('.pageLoader, #page-cover').delay(250).fadeOut( "fast");
-
-    event.preventDefault();
-
-    if (request) {
-        request.abort();
-    }
-
-    var $form = $(this);
-
-    var $inputs = $form.find("input, select, button, textarea");
-
-
-    var serializedData = $form.serialize();
-
-    request = $.ajax({
-        url: "/sell_land.php",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        updateHeaderData();
-        var array = JSON.parse(response);
-
-
-				$.notify({
-					message: array.status,
-					},{
-					type: 'info',
-					delay: 5000,
-					allow_dismiss: true,
-					newest_on_top: true,
-						});
-
-
-			if(array.next == true){
-
-				$( ".sellNotice" ).empty();
-				$( ".sellNotice" ).append(array.soldtoday);
-
-
-
-
-
-				$('form').trigger("reset");
-				location.reload();
-			}
-});	});
-
-
-
-
-
-})(jQuery);
-</script>
-
+			request = $.ajax({url: "/sell_land.php", type: "post", data: $(this).serialize()});
+			request.done(function (response, textStatus, jqXHR){
+				updateHeaderData();
+				var array = JSON.parse(response);
+				$.notify({message: array.status},{type: 'info', delay: 5000, allow_dismiss: true, newest_on_top: true});
+				if(array.next == true){
+					$( ".sellNotice" ).empty();
+					$( ".sellNotice" ).append(array.soldtoday);
+					$('form').trigger("reset");
+					location.reload();
+				}
+			});
+		});
+	})(jQuery);
+	</script>
 
 </div> <!-- end .pageRow -->
 <?php
--- wp-content/themes/wp-bootstrap-starter/pages/explore/explore.php
+++ wp-content/themes/wp-bootstrap-starter/pages/explore/explore.php
@@ -1,7 +1,6 @@
 <?php
 $exploredToday = $userData['explored_today'][0];
 
-
 $perturnm2 = 200-((ceil($ownedland*0.002)));
 if (($perturnm2 < 50) && ($perturnm2 > 25)) {
 	$perturnm2 = 50;
@@ -20,36 +19,27 @@ $maxAmount = floor((20000-$exploredToday)/$perturnm2);
 		You have explored <strong><?php echo number_format($exploredToday, 0, ',', ' '); ?> m<sup>2</sup></strong> today.
 		You can explore an additional <strong class="maxexp"><?php echo number_format(20000-$exploredToday, 0, ',', ' '); ?> m<sup>2</sup></strong> <i>(<?php echo floor((20000-$exploredToday)/(200-((ceil($ownedland*0.002)))));?> turns)</i>
 	<?php endif;?>
-
 </div>
-
 
 <div class="fw-row">
-<form id="exploreform">
-
-<div class="row no-gutters">
-	<div class="col-md-3 no-gutters">
-		<div class="attackDropdown statCol-1 no-gutters" style="width:100%">
-			Turns to explore
-		</div>
-	</div>
-
-	<div class="col-md-9 no-gutters">
+	<form id="exploreform">
 		<div class="row no-gutters">
-			<div class="col-sm-6 bankCol">
-		 		<input class="unitInput" min="0" max="<?php echo $maxAmount;?>" placeholder="Enter amount" type="number" id="turnsinput" name="turns" style="border: none;"/>
+			<div class="col-md-3 no-gutters">
+				<div class="attackDropdown statCol-1 no-gutters" style="width:100%">
+					Turns to explore
+				</div>
 			</div>
-
-			<div id="maxexp" data-max="<?php echo $maxAmount;?>" class="col-sm-6 bankCol maxexp mainSubmit" style="border-top:0px;background-color:rgba(70, 118, 94, 0.8);">
-				ALL TURNS
+			<div class="col-md-9 no-gutters">
+				<div class="row no-gutters">
+					<div class="col-sm-6 bankCol">
+						<input class="unitInput" min="0" max="<?php echo $maxAmount;?>" placeholder="Enter amount" type="number" id="turnsinput" name="turns" style="border: none;"/>
+					</div>
+					<div id="maxexp" data-max="<?php echo $maxAmount;?>" class="col-sm-6 bankCol maxexp mainSubmit" style="border-top:0px;background-color:rgba(70, 118, 94, 0.8);">
+						ALL TURNS
+					</div>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
-
-
-	<input type="submit" value="Explore" class="mainSubmit">
-</form>
-
-
+		<input type="submit" value="Explore" class="mainSubmit">
+	</form>
 </div>
