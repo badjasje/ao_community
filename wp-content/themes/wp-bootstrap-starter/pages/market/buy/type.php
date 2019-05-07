b809028e810bf9e6842ef466c047395a7739bdea
Jaap Broeders <info@jaapbroeders.com>
2019-05-07 13:35:00 +0200

    Fix layout on market when shipping starting bonus

--- wp-content/themes/wp-bootstrap-starter/pages/market/buy/type.php
+++ wp-content/themes/wp-bootstrap-starter/pages/market/buy/type.php
@@ -1,6 +1,7 @@
 <?php
-	foreach($unitTypes as $unitTypeKey => $unitType) :  ?>
-    <div class="tab-pane smallTable marketBuyTable <?=($startingBonus=='shipping'?'withDelay':'')?> <?php echo $activeTab === $unitTypeKey ? 'active' : ''; ?>"
+
+foreach($unitTypes as $unitTypeKey => $unitType) :  ?>
+    <div class="tab-pane smallTable marketBuyTable <?=($startingBonus=='shipping'?'withDelay ':'').($activeTab===$unitTypeKey?'active':'')?>"
          id="<?php echo $unitTypeKey; ?>" role="tabpanel">
         <div class="blockHeader spaceNotice">
             <?php
@@ -18,37 +19,35 @@
                 $housing = 'barracks';
                 $backColor = "126, 100, 68";
             }
-
             echo sprintf('Your empty %s allow you to build a maximum of <span id="'.$unitTypeKey.'spacecount">%d</span> %s units.', $housing, $space[$unitTypeKey] - $usedSpace[$unitTypeKey], strtolower($unitType));
             ?>
         </div>
 
-
-<div class="row unitRow headerRow" style="border-bottom:1px solid #fff;background-color: rgba(<?php echo $backColor;?>, 0.75);">
-	<div class="col-md-2 celBlock nameBlock">
-		Name
-    </div>
-    <div class="col-md-2 celBlock owned">
-		Owned (ordered)
-    </div>
-    <div class="col-md-1 celBlock price">
-		Price
-    </div>
-    <div class="col-md-1 celBlock attacklife">
-		Att / Life
-    </div>
-    <div class="col-md-2 celBlock targets">
-		Targets
-    </div>
-    <?php if($startingBonus == 'shipping'):?>
-    <div class="col-md-1 celBlock delay">
-			Delay
-    </div>
-    <?php endif;?>
-    <div class="col-md-1 celBlock max">Max</div>
-    <div class="col-md-2 celBlock"></div>
-</div> <!-- // Close Unit row -->
-        <?php $count = 0;
+        <div class="row unitRow headerRow" style="border-bottom:1px solid #fff;background-color: rgba(<?php echo $backColor;?>, 0.75);">
+            <div class="col-md-2 celBlock nameBlock">
+                Name
+            </div>
+            <div class="col-md-2 celBlock owned">
+                Owned (ordered)
+            </div>
+            <div class="col-md-1 celBlock price">
+                Price
+            </div>
+            <div class="col-md-1 celBlock attacklife">
+                Att / Life
+            </div>
+            <div class="col-md-2 celBlock targets">
+                Targets
+            </div>
+            <?php if($startingBonus == 'shipping'):?>
+            <div class="col-md-1 celBlock delay">
+                Delay
+            </div>
+            <?php endif;?>
+            <div class="col-md-1 celBlock max">Max</div>
+            <div class="col-md-2 celBlock"></div>
+        </div> <!-- // Close Unit row -->
+        <?php
         foreach($units as $unitKey => $unit) {
             if ($unit['type'] == $unitTypeKey) {
                 include('type/unit.php');
