4af8723ccd39fbecb722ff8fdbe563a83edb20cd
Jaap Broeders <info@jaapbroeders.com>
2019-05-29 13:23:14 +0200

    Codestyle

--- wp-content/themes/wp-bootstrap-starter/pages/dashboard/latest-block.php
+++ wp-content/themes/wp-bootstrap-starter/pages/dashboard/latest-block.php
@@ -30,44 +30,39 @@ $inboxargs = array(
     ),
 );
 $messages = get_posts( $inboxargs );
-
 ?>
 <div class="statusBlock">
 	<div class="row statusTotalRow">
+
 		<div class="col-md-6 col-lg-3 statusRow statCol-4">
-		<div class="blockHeader">Latest orders</div>
+			<div class="blockHeader">Latest orders</div>
+			<?php foreach ($orders as $order):
+				$units_in_this_order = get_post_meta($order->ID,'amount_ordered',true);
+				$order_type = get_post_meta($order->ID,'order_type',true);
 
-		<?php foreach ($orders as $order):
-		   	$units_in_this_order = get_post_meta($order->ID,'amount_ordered',true);
-		   	$order_type = get_post_meta($order->ID,'order_type',true);
+				$userId = $order->post_author;
+				$delivery_time = get_post_meta($order->ID,'delivery_time',true);
 
-		   	$userId = $order->post_author;
-		   	$delivery_time = get_post_meta($order->ID,'delivery_time',true);
-
-
-		   	$timeleft = date('H:i:s', $delivery_time-$timestamp);
-
-		?>
-		<div class="row unitRow">
-			<div class="col-md-4 celBlock nameBlock sea_heading">
-				<?php echo get_the_title($order->ID);?>
-			</div>
-			<div class="col-md-4 celBlock">
-				<span class="columnDataLeft">Ordered</span>
-				<span class="columnDataRight"><?php echo $units_in_this_order;?></span>
-			</div>
-			<div class="col-md-4 celBlock">
-				<span class="columnDataLeft">Time left</span>
-				<span class="columnDataRight"><?php echo $timeleft;?></span>
-			</div>
-		</div>
+				$timeleft = date('H:i:s', $delivery_time-$timestamp);
+				?>
+				<div class="row unitRow">
+					<div class="col-md-4 celBlock nameBlock sea_heading">
+						<?php echo get_the_title($order->ID);?>
+					</div>
+					<div class="col-md-4 celBlock">
+						<span class="columnDataLeft">Ordered</span>
+						<span class="columnDataRight"><?php echo $units_in_this_order;?></span>
+					</div>
+					<div class="col-md-4 celBlock">
+						<span class="columnDataLeft">Time left</span>
+						<span class="columnDataRight"><?php echo $timeleft;?></span>
+					</div>
+				</div>
 			<?php endforeach;?>
 		</div>
 
-
 		<div class="col-md-6 col-lg-3 statusRow statCol-3">
 			<div class="blockHeader">Latest news</div>
-
 			<?php foreach ($posts as $post):
 			   	$units_in_this_order = get_post_meta($order->ID,'amount_ordered',true);
 			   	$order_type = get_post_meta($order->ID,'order_type',true);
@@ -77,57 +72,48 @@ $messages = get_posts( $inboxargs );
 
 
 			   	$timeleft = date('H:i:s', $delivery_time-$timestamp);
-
-			?>
-			<div class="row unitRow">
-				<div class="col-md-12 celBlock">
-					<a href="<?php echo get_the_permalink($post->ID);?>">
-						<?php echo $post->post_title;?>
-					</a>
+				?>
+				<div class="row unitRow">
+					<div class="col-md-12 celBlock">
+						<a href="<?php echo get_the_permalink($post->ID);?>">
+							<?php echo $post->post_title;?>
+						</a>
+					</div>
 				</div>
-			</div>
 			<?php endforeach;?>
 		</div>
-
 
 		<div class="col-md-6 col-lg-3 statusRow statCol-2">
 			<div class="blockHeader">Latest forum topics</div>
-
 			<?php foreach ($topics as $topic):?>
-			<div class="row unitRow">
-				<div class="col-md-12 celBlock">
-					<a href="/forum/topic/<?php echo $topic->id;?>">
-						<?php echo $topic->name;?>
-					</a>
+				<div class="row unitRow">
+					<div class="col-md-12 celBlock">
+						<a href="/forum/topic/<?php echo $topic->id;?>">
+							<?php echo $topic->name;?>
+						</a>
+					</div>
 				</div>
-			</div>
 			<?php endforeach;?>
 		</div>
-
 
 		<div class="col-md-6 col-lg-3 statusRow statCol-1">
 			<div class="blockHeader">Recent conversations</div>
-
 			<?php foreach ($messages as $message):
-				  $messageId = $message->ID;
-
-			?>
-			<div class="row unitRow">
-				<div class="col-md-12 celBlock">
-					<a href="<?php echo get_the_permalink($messageId);?>/#lastrow">
-						<?php if (strlen(get_the_title($messageId)) > 55) {
-	                        echo substr(get_the_title($messageId), 0, 55) . '...'; } else {
-	                        echo get_the_title($messageId);
-						}?>
-					</a>
+				$messageId = $message->ID;
+				?>
+				<div class="row unitRow">
+					<div class="col-md-12 celBlock">
+						<a href="<?php echo get_the_permalink($messageId);?>/#lastrow">
+							<?php if (strlen(get_the_title($messageId)) > 55) {
+								echo substr(get_the_title($messageId), 0, 55) . '...'; } else {
+								echo get_the_title($messageId);
+							}?>
+						</a>
+					</div>
 				</div>
-			</div>
-			<?php endforeach;?>
+				<?php
+			endforeach;?>
 		</div>
 
-
-
-
-	 	</div> <!-- // End row -->
-
+	</div> <!-- // End row -->
  </div>
